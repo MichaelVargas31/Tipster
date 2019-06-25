656c5f5b664580020141d1ad401351ff5f7b7813
Michael Vargas <michaelvargas@fb.com>
2019-06-25 14:14:21 -0700

    Initial Commit

---
+++ Tipster/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tipster
+//
+//  Created by michaelvargas on 6/25/19.
+//  Copyright © 2019 michaelvargas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
